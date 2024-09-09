723f7351f2ecc4eb83bc965c0420fc0daaed2106
Anderson Gaitolini <gaitolini@gmail.com>
2024-09-09 11:14:38 -0300

    Ajustes para obter o Token

--- services/bb/banco_brasil.go
+++ services/bb/banco_brasil.go
@@ -1,10 +1,9 @@
 package bb
 
 import (
+	"api-pix-go/config"
 	"context"
 	"fmt"
-
-	"api-pix-go/config"
 
 	"github.com/go-resty/resty/v2"
 	"golang.org/x/oauth2/clientcredentials"
@@ -16,37 +15,36 @@ func NewBBService() *BBService {
 	return &BBService{}
 }
 
-func (b *BBService) CreatePix(amount string, txid string, chavePix string) (string, error) {
-	// Configuração OAuth2 manual para o Banco do Brasil
+// Função para obter o token OAuth2
+func (b *BBService) getToken() (string, error) {
 	oauthConfig := clientcredentials.Config{
 		ClientID:     config.GetBBClientID(),
 		ClientSecret: config.GetBBClientSecret(),
 		TokenURL:     config.GetBBTokenURL(),
 		Scopes: []string{
-			"cob.write",
-			"cob.read",
-			"cobv.write",
-			"cobv.read",
-			"lotecobv.write",
-			"lotecobv.read",
-			"pix.write",
-			"pix.read",
-			"webhook.read",
-			"webhook.write",
-			"payloadlocation.write",
-			"payloadlocation.read",
+			"cob.write", "cob.read", "cobv.write", "cobv.read",
+			"lotecobv.write", "lotecobv.read", "pix.write", "pix.read",
+			"webhook.read", "webhook.write", "payloadlocation.write", "payloadlocation.read",
 		},
 	}
 
-	// Obter o token OAuth2
 	token, err := oauthConfig.Token(context.Background())
 	if err != nil {
 		return "", fmt.Errorf("Erro ao obter token OAuth2: %v", err)
 	}
+	return token.AccessToken, nil
+}
 
-	// Criar cliente HTTP com Resty e adicionar o token OAuth2 manualmente
-	client := resty.New().
-		SetAuthToken(token.AccessToken) // Adicionar o token OAuth2 no cabeçalho
+// Função para criar o PIX
+func (b *BBService) CreatePix(amount string, txid string, chavePix string) (string, error) {
+	// Obter o token OAuth2
+	token, err := b.getToken()
+	if err != nil {
+		return "", err
+	}
+
+	// Criar cliente HTTP com Resty e adicionar o token OAuth2
+	client := resty.New().SetAuthToken(token)
 
 	// Corpo da requisição para criar o PIX
 	requestBody := map[string]interface{}{
@@ -62,7 +60,7 @@ func (b *BBService) CreatePix(amount string, txid string, chavePix string) (stri
 	// Fazer a requisição para criar o PIX
 	response, err := client.R().
 		SetHeader("Content-Type", "application/json").
-		SetHeader("developer_application_key", config.GetBBApplicationKey()). // Chave do Banco do Brasil
+		SetHeader("developer_application_key", config.GetBBApplicationKey()).
 		SetBody(requestBody).
 		Post(fmt.Sprintf("%s/cob/%s", config.GetBBApiURL(), txid))
 
@@ -73,28 +71,21 @@ func (b *BBService) CreatePix(amount string, txid string, chavePix string) (stri
 	return response.String(), nil
 }
 
+// Função para consultar o status do PIX
 func (b *BBService) GetPixStatus(txid string) (string, error) {
-	// Configuração OAuth2 manual
-	oauthConfig := clientcredentials.Config{
-		ClientID:     config.GetBBClientID(),
-		ClientSecret: config.GetBBClientSecret(),
-		TokenURL:     config.GetBBTokenURL(),
-	}
-
 	// Obter o token OAuth2
-	token, err := oauthConfig.Token(context.Background())
+	token, err := b.getToken()
 	if err != nil {
-		return "", fmt.Errorf("Erro ao obter token OAuth2: %v", err)
+		return "", err
 	}
 
-	// Criar cliente HTTP com Resty e adicionar o token OAuth2 manualmente
-	client := resty.New().
-		SetAuthToken(token.AccessToken) // Adicionar o token OAuth2 no cabeçalho
+	// Criar cliente HTTP com Resty e adicionar o token OAuth2
+	client := resty.New().SetAuthToken(token)
 
 	// Fazer a requisição para obter o status do PIX
 	response, err := client.R().
 		SetHeader("Content-Type", "application/json").
-		SetHeader("developer_application_key", config.GetBBApplicationKey()). // Chave do Banco do Brasil
+		SetHeader("developer_application_key", config.GetBBApplicationKey()).
 		Get(fmt.Sprintf("%s/cob/%s", config.GetBBApiURL(), txid))
 
 	if err != nil {
